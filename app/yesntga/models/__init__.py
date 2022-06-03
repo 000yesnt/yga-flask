21da4d08230b0a5e89a85a402fa83a69093602e1
000yesnt <49329895+000yesnt@users.noreply.github.com>
2022-06-03 20:15:08 -0300

    i accidentally made an infinie loop

--- app/yesntga/models/__init__.py
+++ app/yesntga/models/__init__.py
@@ -3,17 +3,16 @@ from yesntga.models import depot
 from sqlalchemy.exc import OperationalError
 from yesntga.util.log import lg 
 conn_successful = False 
-conn_fail = False
 conn_attempts = 0
-while not conn_successful or not conn_fail:
+while not conn_successful:
     try:
         db.create_all()
         conn_successful = True
+        break
     except OperationalError as e:
         if conn_attempts >= 3:
             lg.critical('All attempts to connect to the database failed.')
             lg.exception(e)
-            conn_fail = True
             break
         lg.critical(f"Failed to connect to database (attempt {conn_attempts}). Retrying...")
         conn_attempts += 1
