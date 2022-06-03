35463b7502e8323127697d17c28fe8854ad432cd
000yesnt <49329895+000yesnt@users.noreply.github.com>
2022-06-03 19:35:34 -0300

    Enhance CLI logging

--- app/yesntga/util/log.py
+++ app/yesntga/util/log.py
@@ -2,10 +2,14 @@ import logging.handlers
 from logging import DEBUG
 lg = logging.getLogger(__name__)
 
-handler = logging.handlers.TimedRotatingFileHandler(filename='core.log', when='H', interval=1, backupCount=4,
+file_handler = logging.handlers.TimedRotatingFileHandler(filename='core.log', when='H', interval=1, backupCount=4,
                                                     encoding='utf-8', delay=False)
-fmtr = logging.Formatter(fmt="[%(levelname)s @ %(module)s/%(process)d | %(asctime)s] %(message)s")
-handler.setFormatter(fmtr)
-lg.addHandler(handler)
-lg.addHandler(logging.StreamHandler())
+std_handler = logging.StreamHandler()
+
+formatter = logging.Formatter(fmt="[%(levelname)s @ %(module)s/%(process)d | %(asctime)s] %(message)s")
+std_handler.setFormatter(file_formatter)
+file_handler.setFormatter(formatter)
+
+lg.addHandler(file_handler)
+lg.addHandler(std_handler)
 lg.setLevel(DEBUG)
