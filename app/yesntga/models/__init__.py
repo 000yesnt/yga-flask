d044959c91d23c05f782e1c2dde3283427b45158
000yesnt <49329895+000yesnt@users.noreply.github.com>
2022-06-03 19:35:51 -0300

    Database auto reconnect

--- app/yesntga/models/__init__.py
+++ app/yesntga/models/__init__.py
@@ -1,3 +1,19 @@
 from yesntga import db
 from yesntga.models import depot
-db.create_all()
+from sqlalchemy.exc import OperationalError
+from yesntga.util.log import lg 
+conn_successful = False 
+conn_fail = False
+conn_attempts = 0
+while not conn_successful or not conn_fail:
+    try:
+        db.create_all()
+        conn_successful = True
+    except OperationalError as e:
+        if conn_attempts >= 3:
+            lg.critical('All attempts to connect to the database failed.')
+            lg.exception(e)
+            conn_fail = True
+            break
+        lg.critical(f"Failed to connect to database (attempt {conn_attempts}). Retrying...")
+        conn_attempts += 1
