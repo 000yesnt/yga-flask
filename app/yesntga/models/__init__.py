87536139ac3bdfe3e8d4dc96a8b0fe4e0eb892ed
000yesnt <49329895+000yesnt@users.noreply.github.com>
2022-06-03 20:25:40 -0300

    connect delay + __all__ hack

--- app/yesntga/models/__init__.py
+++ app/yesntga/models/__init__.py
@@ -1,7 +1,10 @@
+__all__ = ['depot'] # Hack
+
 from yesntga import db
-from yesntga.models import depot
+from yesntga.models import *
 from sqlalchemy.exc import OperationalError
 from yesntga.util.log import lg 
+from time import sleep
 conn_successful = False 
 conn_attempts = 0
 while not conn_successful:
@@ -13,6 +16,7 @@ while not conn_successful:
         if conn_attempts >= 3:
             lg.critical('All attempts to connect to the database failed.')
             lg.exception(e)
-            break
-        lg.critical(f"Failed to connect to database (attempt {conn_attempts}). Retrying...")
+            raise e
+        lg.critical(f"Failed to connect to database (attempt {conn_attempts}). Retrying in 3 seconds...")
         conn_attempts += 1
+        sleep(3)
